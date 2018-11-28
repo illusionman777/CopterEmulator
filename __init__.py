46d891059aa2f726436c9f7955358624a10a787e
illusionman777 <VSRyabov1@gmail.com>
2018-11-28 23:10:47 +0200

    Bug fix

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
+from . import extrafunctions
+from . import graphics
+from . import gui
+from . import json_serializer
+from . import logger
+from . import simulation
+from . import study_module
+from .c_emulator import cEmulator
+
 name = "CopterEmulator"
